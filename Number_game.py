88c44e124bba29c944987fd3eb4b8ba4e2f56a3c
Raghav <trscs.raghav@gmail.com>
2026-04-27 16:39:46 -0700

    Add files via upload
    up dated number game with high score tracker. Just make sure before you play the game for the first time you change whatever is in the text file to a high number(eg: 100000000)

--- Number_game.py
+++ Number_game.py
@@ -2,8 +2,19 @@ import random
 x=(random. randrange(0,1000))
 y= int(input( "guess a number between 0 and 1000:"))
 g_count=0
+def update_score(g_count):
+    with open('num_game_hs.txt', 'r') as f:
+        content = f.read() 
+        if g_count < int(content):
+            with open('num_game_hs.txt', 'w') as f:
+                f.write(str(g_count))
+                print('new highscore!', g_count)
+        else:
+            print('Current highscore is: ', content)
+           
+
 while x!=y:
-    g_count=g_count + 1
+    g_count+=1
     print('guess count:', g_count)
     if x>y>x-100:
         print("guess a little higher")
@@ -17,3 +28,5 @@ while x!=y:
 
 if x==y:
     print('You Win')
+    print('Your score is: ', g_count)
+    update_score(g_count)
